91fc96dd8be9d64c723498aec8ab0cd58181f7ab
Kohei Yoshida <kohei.yoshida@collabora.com>
2013-09-10 21:09:24 -0400

    First step toward splitting the opencl code into own shared library.
    Change-Id: I44fa3ded8d48b2972af17b78ab6c0af03e024f36

--- Repository.mk
+++ Repository.mk
@@ -155,6 +155,7 @@ $(eval $(call gb_Helper_register_libraries_for_install,OOOLIBS,calc, \
 	scfilt \
 	scui \
 	solver \
+	$(if $(ENABLE_OPENCL),scopencl) \
 	$(if $(DISABLE_SCRIPTING),,vbaobj) \
 	$(if $(ENABLE_TELEPATHY),tubes) \
 ))
