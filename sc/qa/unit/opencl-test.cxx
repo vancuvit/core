a338b130af399231098daa8e98f22b82dc23aac1
yiming ju <yiming@multicorewareinc.com>
2013-11-04 15:48:10 +0800

    GPU Calc: unit test cases for PV
    Need open macro NO_FALLBACK_TO_SWINTERP in formulagroupcl.cxx for test
    
    AMLOEXT-73 BUG
    
    Change-Id: I2ed3e1dfa07aac5f28942a1a2c94826757c9d0cd
    Signed-off-by: haochen <haochen@multicorewareinc.com>
    Signed-off-by: I-Jui (Ray) Sung <ray@multicorewareinc.com>

--- sc/qa/unit/opencl-test.cxx
+++ sc/qa/unit/opencl-test.cxx
@@ -126,6 +126,7 @@ public:
     void testFinancialCoupnumFormula();
     void testMathFormulaSinh();
     void testMathFormulaAbs();
+    void testFinacialPVFormula();
     CPPUNIT_TEST_SUITE(ScOpenclTest);
     CPPUNIT_TEST(testSharedFormulaXLS);
     CPPUNIT_TEST(testFinacialFormula);
@@ -181,6 +182,7 @@ public:
     CPPUNIT_TEST(testFinancialCoupnumFormula);
     CPPUNIT_TEST(testMathFormulaSinh);
     CPPUNIT_TEST(testMathFormulaAbs);
+    CPPUNIT_TEST(testFinacialPVFormula);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -1237,6 +1239,28 @@ void ScOpenclTest::testFinacialDISCFormula()
         double fLibre = pDoc->GetValue(ScAddress(5, i, 0));
         double fExcel = pDocRes->GetValue(ScAddress(5, i, 0));
         //CPPUNIT_ASSERT_EQUAL(fExcel, fLibre);
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(fExcel, fLibre, fabs(0.0001*fExcel));
+    }
+    xDocSh->DoClose();
+    xDocShRes->DoClose();
+}
+//[AMLOEXT-73]
+void ScOpenclTest:: testFinacialPVFormula()
+{
+    if (!detectOpenCLDevice())
+        return;
+    ScDocShellRef xDocSh = loadDoc("opencl/financial/PV.", XLS);
+    ScDocument *pDoc = xDocSh->GetDocument();
+    CPPUNIT_ASSERT(pDoc);
+    enableOpenCL();
+    pDoc->CalcAll();
+    ScDocShellRef xDocShRes = loadDoc("opencl/financial/PV.", XLS);
+    ScDocument *pDocRes = xDocShRes->GetDocument();
+    CPPUNIT_ASSERT(pDocRes);
+    for (SCROW i = 0; i <= 9; ++i)
+    {
+        double fLibre = pDoc->GetValue(ScAddress(5, i, 0));
+        double fExcel = pDocRes->GetValue(ScAddress(5, i, 0));
         CPPUNIT_ASSERT_DOUBLES_EQUAL(fExcel, fLibre, fabs(0.0001*fExcel));
     }
     xDocSh->DoClose();
