887a919d7ae3943cd8bbce7b087c9211bb74718f
fengzeng <fengzeng@multicorewareinc.com>
2013-11-05 16:49:26 +0800

    GPU Calc: unit test cases for SQRT
    Need open macro NO_FALLBACK_TO_SWINTERP in formulagroupcl.cxx for test
    
    AMLOEXT-62 BUG
    
    Change-Id: Ibf5a4789deebf75cd017cc015bc2f8ae2618f6f0
    Signed-off-by: haochen <haochen@multicorewareinc.com>
    Signed-off-by: I-Jui (Ray) Sung <ray@multicorewareinc.com>

--- sc/qa/unit/opencl-test.cxx
+++ sc/qa/unit/opencl-test.cxx
@@ -145,6 +145,7 @@ public:
     void testFinacialNPERFormula();
     void testStatisticalFormulaNormdist();
     void testMathFormulaArcCos();
+    void testMathFormulaSqrt();
     CPPUNIT_TEST_SUITE(ScOpenclTest);
     CPPUNIT_TEST(testSharedFormulaXLS);
     CPPUNIT_TEST(testFinacialFormula);
@@ -219,6 +220,7 @@ public:
     CPPUNIT_TEST(testFinacialNPERFormula);
     CPPUNIT_TEST(testStatisticalFormulaNormdist);
     CPPUNIT_TEST(testMathFormulaArcCos);
+    CPPUNIT_TEST(testMathFormulaSqrt);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -1266,6 +1268,28 @@ void ScOpenclTest::testMathFormulaTanH()
     enableOpenCL();
     pDoc->CalcAll();
     ScDocShellRef xDocShRes = loadDoc("opencl/math/tanh.", XLS);
+    ScDocument* pDocRes = xDocShRes->GetDocument();
+    CPPUNIT_ASSERT(pDocRes);
+    for (SCROW i = 0; i <= 15; ++i)
+    {
+        double fLibre = pDoc->GetValue(ScAddress(1,i,0));
+        double fExcel = pDocRes->GetValue(ScAddress(1,i,0));
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(fExcel, fLibre, fabs(0.0001*fExcel));
+    }
+    xDocSh->DoClose();
+    xDocShRes->DoClose();
+}
+//[AMLOEXT-62]
+void ScOpenclTest::testMathFormulaSqrt()
+{
+    if (!detectOpenCLDevice())
+            return;
+    ScDocShellRef xDocSh = loadDoc("opencl/math/sqrt.", XLS);
+    ScDocument* pDoc = xDocSh->GetDocument();
+    CPPUNIT_ASSERT(pDoc);
+    enableOpenCL();
+    pDoc->CalcAll();
+    ScDocShellRef xDocShRes = loadDoc("opencl/math/sqrt.", XLS);
     ScDocument* pDocRes = xDocShRes->GetDocument();
     CPPUNIT_ASSERT(pDocRes);
     for (SCROW i = 0; i <= 15; ++i)
