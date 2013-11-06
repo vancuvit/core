f3d49237f27d4df0abcdf9be9bfc20536ac41a77
minwang <min@multicorewareinc.com>
2013-11-06 14:54:09 +0800

    GPU Calc: unit test cases for COUPNCD
    Need open macro NO_FALLBACK_TO_SWINTERP in formulagroupcl.cxx for test
    
    AMLOEXT-136 BUG
    
    Change-Id: I00478c7094240f61c4f316eac833756a0ad8edc2
    Signed-off-by: haochen <haochen@multicorewareinc.com>
    Signed-off-by: I-Jui (Ray) Sung <ray@multicorewareinc.com>

--- sc/qa/unit/opencl-test.cxx
+++ sc/qa/unit/opencl-test.cxx
@@ -163,6 +163,7 @@ public:
     void testFinacialPriceDiscFormula();
     void testFinancialDBFormula();
     void testFinancialCouppcdFormula();
+    void testFinancialCoupncdFormula();
     CPPUNIT_TEST_SUITE(ScOpenclTest);
     CPPUNIT_TEST(testSharedFormulaXLS);
     CPPUNIT_TEST(testFinacialFormula);
@@ -255,6 +256,7 @@ public:
     CPPUNIT_TEST(testFinacialPriceDiscFormula);
     CPPUNIT_TEST(testFinancialDBFormula);
     CPPUNIT_TEST(testFinancialCouppcdFormula);
+    CPPUNIT_TEST(testFinancialCoupncdFormula);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -2673,6 +2675,28 @@ void ScOpenclTest::testStatisticalFormulaConfidence()
     {
         double fLibre = pDoc->GetValue(ScAddress(3,i,0));
         double fExcel = pDocRes->GetValue(ScAddress(3,i,0));
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(fExcel, fLibre, fabs(0.0001*fExcel));
+    }
+    xDocSh->DoClose();
+    xDocShRes->DoClose();
+}
+//[AMLOEXT-136]
+void ScOpenclTest::testFinancialCoupncdFormula()
+{
+    if (!detectOpenCLDevice())
+        return;
+    ScDocShellRef xDocSh = loadDoc("opencl/financial/Coupncd.", XLS);
+    ScDocument *pDoc = xDocSh->GetDocument();
+    CPPUNIT_ASSERT(pDoc);
+    enableOpenCL();
+    pDoc->CalcAll();
+    ScDocShellRef xDocShRes = loadDoc("opencl/financial/Coupncd.", XLS);
+    ScDocument *pDocRes = xDocShRes->GetDocument();
+    CPPUNIT_ASSERT(pDocRes);
+    for (SCROW i = 0; i <= 9; ++i)
+    {
+        double fLibre = pDoc->GetValue(ScAddress(4, i, 0));
+        double fExcel = pDocRes->GetValue(ScAddress(4, i, 0));
         CPPUNIT_ASSERT_DOUBLES_EQUAL(fExcel, fLibre, fabs(0.0001*fExcel));
     }
     xDocSh->DoClose();
