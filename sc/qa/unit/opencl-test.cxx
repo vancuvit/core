236c3ca4f1af668be8357e8551e231087734be44
mingli <mingli@multicorewareinc.com>
2013-11-07 14:24:59 +0800

    GPU Calc: unit test cases for GAMMADIST
    Need open macro NO_FALLBACK_TO_SWINTERP in formulagroupcl.cxx for test
    
    AMLOEXT-157 BUG
    
    Change-Id: I7f97ee47050378fa411b5e635141847929c55b46
    Signed-off-by: haochen <haochen@multicorewareinc.com>
    Signed-off-by: I-Jui (Ray) Sung <ray@multicorewareinc.com>

--- sc/qa/unit/opencl-test.cxx
+++ sc/qa/unit/opencl-test.cxx
@@ -177,6 +177,7 @@ public:
     void testMathFormulaBitAnd();
     void testStatisticalFormulaForecast();
     void testStatisticalFormulaLogNormDist();
+    void testStatisticalFormulaGammaDist();
     CPPUNIT_TEST_SUITE(ScOpenclTest);
     CPPUNIT_TEST(testSharedFormulaXLS);
     CPPUNIT_TEST(testFinacialFormula);
@@ -283,6 +284,7 @@ public:
     CPPUNIT_TEST(testMathFormulaBitAnd);
     CPPUNIT_TEST(testStatisticalFormulaForecast);
     CPPUNIT_TEST(testStatisticalFormulaLogNormDist);
+    CPPUNIT_TEST(testStatisticalFormulaGammaDist);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -3041,6 +3043,30 @@ void ScOpenclTest::testMathFormulaBitAnd()
     {
         double fLibre = pDoc->GetValue(ScAddress(2,i,0));
         double fExcel = pDocRes->GetValue(ScAddress(2,i,0));
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(fExcel, fLibre, fabs(0.0001*fExcel));
+    }
+    xDocSh->DoClose();
+    xDocShRes->DoClose();
+}
+//[AMLOEXT-157]
+void ScOpenclTest::testStatisticalFormulaGammaDist()
+{
+    if (!detectOpenCLDevice())
+        return;
+
+    ScDocShellRef xDocSh = loadDoc("opencl/statistical/GammaDist.", XLS);
+    ScDocument* pDoc = xDocSh->GetDocument();
+    CPPUNIT_ASSERT(pDoc);
+    enableOpenCL();
+    pDoc->CalcAll();
+    ScDocShellRef xDocShRes = loadDoc("opencl/statistical/GammaDist.", XLS);
+    ScDocument* pDocRes = xDocShRes->GetDocument();
+    CPPUNIT_ASSERT(pDocRes);
+    // Check the results of formula cells in the shared formula range.
+    for (SCROW i = 1; i <= 19; ++i)
+    {
+        double fLibre = pDoc->GetValue(ScAddress(4,i,0));
+        double fExcel = pDocRes->GetValue(ScAddress(4,i,0));
         CPPUNIT_ASSERT_DOUBLES_EQUAL(fExcel, fLibre, fabs(0.0001*fExcel));
     }
     xDocSh->DoClose();
