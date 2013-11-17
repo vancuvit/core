7f723c49b1f64253c8e43f5d5093e5f9700ec5de
minwang <min@multicorewareinc.com>
2013-11-17 14:26:52 +0800

    GPU Calc: unit test cases for AND
    Turn NO_FALLBACK_TO_SWINTERP on  in formulagroupcl.cxx for test
    
    AMLOEXT-217 BUG
    
    Change-Id: I768e3b58e37d56c527f364aab54b88ecfe8c0907
    Signed-off-by: haochen <haochen@multicorewareinc.com>
    Signed-off-by: I-Jui (Ray) Sung <ray@multicorewareinc.com>

--- sc/source/core/tool/token.cxx
+++ sc/source/core/tool/token.cxx
@@ -1469,6 +1469,7 @@ void ScTokenArray::CheckToken( const FormulaToken& r )
             case ocSumXMY2:
             case ocStDevP:
             case ocCovar:
+            case ocAnd:
             // Don't change the state.
             break;
             default:
